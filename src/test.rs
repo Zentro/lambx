9615942eb0844d36def3fb73551bfd3887bc2e28
Phuc Dang <peaetchyousee@gmail.com>
2023-10-25 14:55:45 -0500

    Slight changes to the API: Follows the document more closely. Cleaned up the chain updates, which no longer requires the concat() call Comments added for future me :3

--- src/test.rs
+++ src/test.rs
@@ -2,7 +2,7 @@ use crate::xeddsa;
 
 use rand::{rngs::StdRng, SeedableRng, Rng};
 use x25519_dalek::{StaticSecret, PublicKey};
-use hex::{self, ToHex};
+use hex;
 
 pub fn test() {
     let r = StdRng::from_entropy();
@@ -11,19 +11,19 @@ pub fn test() {
     let r2 = StdRng::from_entropy();
     let bob_secret = StaticSecret::random_from_rng(r2);
     let bob_public = PublicKey::from(&bob_secret);
-    let x = xeddsa::XEdDSA::new(alice_secret);
-    for i in 0..10 {
+    let x = xeddsa::XEdDSA::new();
+    for _ in 0..10 {
         let msg = rand::thread_rng().gen::<[u8; 16]>();
         let nonce = rand::thread_rng().gen::<[u8; 16]>();
-        let sig = x.sign(&msg, &nonce);
+        let sig = x.sign(alice_secret.to_bytes(),&msg, &nonce);
         println!("Alice sent message {}", hex::encode(msg));
         println!("Alice's signature on the message {}", hex::encode(sig));
-        if x.verify(alice_public, &msg, sig) {
+        if x.verify(alice_public.to_bytes(), &msg, sig) {
             println!("Alice succesfully verified with her public key.");
         } else {
             println!("Alice failed to verify with her public key.");
         }
-        if x.verify(bob_public, &msg, sig) {
+        if x.verify(bob_public.to_bytes(), &msg, sig) {
             println!("Bob should not be able to do this.");
         } else {
             println!("Bob tried to verify Alice's signature using his public key and FAILED.");
--- src/xeddsa.rs
+++ src/xeddsa.rs
@@ -1,26 +1,34 @@
 use sha2::{Sha512, Digest};
 use bnum::types::U256;
 use curve25519_dalek::{edwards::{EdwardsPoint, CompressedEdwardsY}, Scalar, MontgomeryPoint, scalar::clamp_integer};
-use x25519_dalek::{PublicKey, StaticSecret};
 
 pub struct XEdDSA {
     prime: U256,
-    sk_scalar: Scalar,
 }
 
 #[allow(non_snake_case)]
 #[allow(deprecated)]
 impl XEdDSA {
-    pub fn new(sk: StaticSecret) -> Self {
-        let prime = (U256::ONE << 255) - U256::from_str_radix("19", 10).unwrap();
+    /**
+     * Instance of an XEdDSA, will likely change to be non object
+     */
+    pub fn new() -> Self {
         Self {
-            prime,
-            sk_scalar: Scalar::from_bits(clamp_integer(sk.to_bytes())),
+            prime: (U256::ONE << 255) - U256::from_str_radix("19", 10).unwrap(),
         }
     }
 
-    pub fn sign(&self, msg: &[u8], nonce: &[u8]) -> [u8; 64] {
-        let (ed_pk, ed_sk) = self.calculate_key_pair(self.sk_scalar);
+    /**
+     * Signs a message along with a secret key. Nonce NEEDS to be randomly generated
+     * preferably by a cryptographically secure PRG.
+     * 
+     * Returns a 64 byte signature
+     */
+    pub fn sign(&self, sk: [u8; 32], msg: &[u8], nonce: &[u8]) -> [u8; 64] {
+        // Scalar value derived from X25519's way of clamping byte array and
+        // directly using it without (mod q)
+        let sk_scalar = Scalar::from_bits(clamp_integer(sk));
+        let (ed_pk, ed_sk) = self.calculate_key_pair(sk_scalar);
         
         let concat1 = [&ed_sk, msg, nonce].concat();
         let r0 = self.hash_i(1, concat1.as_slice());
@@ -28,9 +36,10 @@ impl XEdDSA {
         
         let R = EdwardsPoint::mul_base(&r).compress().to_bytes();
 
-        let concat2 = [&R, &ed_pk, msg].concat();
         let hash: [u8; 64] = Sha512::new()
-            .chain_update(concat2.as_slice())
+            .chain_update(R)
+            .chain_update(ed_pk)
+            .chain_update(msg)
             .finalize()
             .as_slice()
             .try_into()
@@ -42,9 +51,12 @@ impl XEdDSA {
         to_ret
     }
 
-    pub fn verify(&self, pk: PublicKey, msg: &[u8], sig: [u8; 64]) -> bool {
-        let mont_pk = pk.to_bytes();
-        let pk_int: U256 = U256::from_le_slice(&mont_pk).unwrap();
+    /**
+     * Verifies a message to its signature using a public key generated
+     * from the secret key derive from the X25519-Dalek functions. 
+     */
+    pub fn verify(&self, pk: [u8; 32], msg: &[u8], sig: [u8; 64]) -> bool {
+        let pk_int: U256 = U256::from_le_slice(&pk).unwrap();
         // Separate R from sig
         let mut R: [u8; 32] = [0; 32];
         R.clone_from_slice(&sig[0..32]);
@@ -61,7 +73,7 @@ impl XEdDSA {
            s_int >= (U256::ONE << 253) {
             return false;
         }
-        let A = match (MontgomeryPoint{0:mont_pk}).to_edwards(0) {
+        let A = match (MontgomeryPoint{0:pk}).to_edwards(0) {
             Some(res) => res,
             None => return false, // Not on the curve
         };
@@ -84,6 +96,9 @@ impl XEdDSA {
         }
     }
 
+    /**
+     * Acts almost like a keyed hash function (except not)
+     */
     fn hash_i(&self, i: u128, msg: &[u8]) -> [u8; 64]{
         let prefix = U256::MAX.sub(i.into());
         let hash = Sha512::new()
